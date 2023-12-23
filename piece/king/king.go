014bed2c62fc4bd0d3c1a5583661ec498ca7c6ad
Vladimir Ljubicic <vladimir.ljubicic87@gmail.com>
2023-12-23 16:36:44 +0100

    Update kings movement engine

--- piece/king/king.go
+++ piece/king/king.go
@@ -2,14 +2,36 @@ package king
 
 import (
 	"github.com/chess/board"
+	"github.com/chess/board/grid"
 	"github.com/chess/piece"
+	"github.com/samber/lo"
 )
 
 type King struct{}
 
 func (k King) UpdateLegalMoves(p *piece.Piece, b board.Board) {
-	//	This will be by far the trickiest one. King cannot move into check, so we need
-	//	to check if any other opposing piece has kings destination as a Legal move.
+	var moves []grid.Cell
 
-	return
+	moves = append(moves,
+		p.Position.Up(),
+		p.Position.Down(),
+		p.Position.Left(),
+		p.Position.Right(),
+		p.Position.Up().Right(),
+		p.Position.Up().Left(),
+		p.Position.Down().Right(),
+		p.Position.Down().Left(),
+	)
+
+	opposingPieces := lo.Filter(b.Pieces, func(pc piece.Piece, _ int) bool {
+		return pc.Color != p.Color
+	})
+
+	for _, op := range opposingPieces {
+		diff, _ := lo.Difference(moves, op.LegalMoves)
+		moves = diff
+	}
+
+	// if len(moves) == 0 signify checkmate , otherwise =>
+	p.LegalMoves = moves
 }
